6c9e84c4ff49b9c089be79b644a4863830e6d921
Carl-Philip Hänsch <carli@launix.de>
2023-01-11 20:20:56 +0100

    Zwischenstand

--- src/HttpServer.hpp
+++ src/HttpServer.hpp
@@ -1,22 +1,67 @@
-#ifndef HTTPSERVER_HPP
-#define HTTPSERVER_HPP
-
 #include <string>
 #include <iostream>
-#include <boost/asio.hpp>
+#include <uv.h>
 
-using namespace boost::asio;
+class HttpServer {
+ public:
+  HttpServer(int port, bool isSecure)
+    : port_(port),
+      isSecure_(isSecure) {
+  }
 
-class HttpServer
-{
-public:
-    HttpServer(int port, bool isSecure);
+  void start() {
+    uv_loop_t* loop = uv_default_loop();
+    uv_tcp_t server;
+    uv_tcp_init(loop, &server);
+    uv_ip4_addr("0.0.0.0", port_, &addr_);
+    uv_tcp_bind(&server, (const struct sockaddr*)&addr_, 0);
 
-    void open();
+    uv_listen((uv_stream_t*) &server, 128, onConnection);
 
-private:
-    int m_port;
-    bool m_isSecure;
+    uv_run(loop, UV_RUN_DEFAULT);
+  }
+
+ private:
+  static void onConnection(uv_stream_t* server, int status) {
+    if (status < 0) {
+      std::cerr << "Error on connection: " << uv_strerror(status);
+      return;
+    }
+
+    uv_tcp_t* client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+    uv_tcp_init(uv_default_loop(), client);
+    if (uv_accept(server, (uv_stream_t*)client) == 0) {
+      uv_read_start((uv_stream_t*)client, onAlloc, onRead);
+    } else {
+      uv_close((uv_handle_t*)client, NULL);
+    }
+  }
+
+  void onSecureConnection(uv_stream_t* server, int status) {
+    // TODO: Implement secure connection
+  }
+
+  static void onRead(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf) {
+    if (nread < 0) {
+      if (nread != UV_EOF)
+        std::cerr << "Error on read: " << uv_strerror(nread);
+      uv_close((uv_handle_t*)client, NULL);
+    } else if (nread > 0) {
+      std::string response = "Hello\n";
+      uv_write_t* writeReq = (uv_write_t*)malloc(sizeof(uv_write_t));
+      uv_buf_t resBuf = uv_buf_init(const_cast<char*>(response.data()), response.size());
+      uv_write(writeReq, client, &resBuf, 1, NULL);
+    }
+
+    free(buf->base);
+  }
+
+  static void onAlloc(uv_handle_t* client, size_t suggestedSize, uv_buf_t* buf) {
+    buf->base = (char*)malloc(suggestedSize);
+    buf->len = suggestedSize;
+  }
+
+  int port_;
+  bool isSecure_;
+  struct sockaddr_in addr_;
 };
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,22 @@
 
 void printHelp()
 {
-    std::cout << "Usage: HttpServer -p [PORT] --secure --help" << std::endl;
+    std::cout << "Usage: HttpServer -p [PORT] --secure -c [IP:PORT] --help" << std::endl;
     std::cout << "-p [PORT], --port [PORT]\tSpecifies the port to listen on" << std::endl;
     std::cout << "--secure\t\t\tEnables HTTPS (default is HTTP)" << std::endl;
+    std::cout << "-c [IP:PORT], --connect [IP:PORT]\tSpecifies a connection to another node" << std::endl;
     std::cout << "-h, --help\t\t\tPrints this help message" << std::endl;
 }
-
+    
 int main(int argc, char *argv[])
-{
+{   
     int port = 3877;
+    std::string connectIP;
+    int connectPort = 3877;
     bool isSecure = false;
-
+    
     for (int i = 0; i < argc; i++)
-    {
+    {   
         std::string arg = argv[i];
 
         if (arg == "-p" || arg == "--port")
@@ -28,15 +31,22 @@ int main(int argc, char *argv[])
         {
             isSecure = true;
         }
-        else if (arg == "-h" || arg == "--help")
+        else if (arg == "-c" || arg == "--connect")
         {
-            printHelp();
+            std::string connectIP_and_Port = argv[i + 1];
+            std::string connectIP = connectIP_and_Port.substr(0, connectIP_and_Port.find(":"));
+            int connectPort = std::atoi(connectIP_and_Port.substr(connectIP_and_Port.find(":") + 1).c_str());
+        }
+        else if (arg == "-h" || arg == "--help")
+        { 
+            printHelp(); 
             return 0;
         }
-    }
+    }   
 
+    // connectIP, connectPort
     HttpServer server(port, isSecure);
-    server.open();
+    server.start();
 
     return 0;
 }
