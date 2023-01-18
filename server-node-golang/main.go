c33ff86b57b42d9f52923d307b159cd5a4c1ca33
Carl-Philip Hänsch <carli@launix.de>
2023-01-18 15:26:14 +0100

    GPLv3

--- server-node-golang/main.go
+++ server-node-golang/main.go
@@ -1,4 +1,20 @@
 /*
+Copyright (C) 2023  Carl-Philip Hänsch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+/*
 	cpdb smart clusterable distributed database working best on nvme
 
 	https://pkelchte.wordpress.com/2013/12/31/scm-go/
@@ -9,6 +25,11 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Print(`cpdb Copyright (C) 2023   Carl-Philip Hänsch
+    This program comes with ABSOLUTELY NO WARRANTY;
+    This is free software, and you are welcome to redistribute it
+    under certain conditions;
+`)
 	// define user specific functions
 	globalenv.vars["print"] = func (a ...scmer) scmer {
 		for _, s := range a {
