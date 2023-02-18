ef90ccea388aabc2ea7f1d89b281fc73a5e02001
Carl-Philip Hänsch <carli@launix.de>
2023-02-18 16:17:16 +0100

    Refactor into multiple files

---
+++ lib/queryplan.scm
@@ -0,0 +1,56 @@
+/*
+Copyright (C) 2023  Carl-Philip Hänsch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/* build queryplan from parsed query */
+(define build_queryplan (lambda (tables fields) (begin
+	/* tables: '('(alias schema tbl) ...) */
+	/* fields: '(colname expr ...) */
+	/* TODO: WHERE, GROUP, HAVING, ORDER, LIMIT */
+	/* expressions will use (get_column tblvar col) for reading from columns. we have to replace it with the correct variable */
+
+	/* returns a list of '(tblvar col) */
+	(define extract_columns (lambda (expr) (match expr
+		'((symbol get_column) tblvar col) '('(tblvar col))
+		(cons sym args) /* function call */ (merge (map args extract_columns)) /* TODO: use collector */
+		'()
+	)))
+
+	/* changes (get_column tblvar col) into its counterpart */
+	(define replace_columns (lambda (expr) (match expr
+		'((symbol get_column) tblvar col) (symbol col) /* TODO: rename in outer scans */
+		(cons sym args) /* function call */ (cons sym (map args replace_columns))
+		expr /* literals */
+	)))
+
+	/* columns: '('(tblalias colname) ...) */
+	(set columns (merge (extract_assoc fields extract_columns)))
+
+	/* TODO: sort tables according to join plan */
+	(define build_scan (lambda (tables)
+		(match tables
+			'('("1x1" "system" "1x1")) '((symbol "resultrow") (cons (symbol "list") fields))
+			(cons '(alias schema tbl) tables) /* outer scan */
+				'((quote scan) schema tbl
+					'((quote lambda) '() (quote true)) /* TODO: filter */
+					/* todo filter columns for alias */
+					'((quote lambda) (map columns (lambda(column) (match column '(tblvar colname) (symbol colname)))) (build_scan tables))
+					/* TODO: reduce+neutral */)
+			'() /* final inner */ '((symbol "resultrow") (cons (symbol "list") (map_assoc fields replace_columns)))
+		)
+	))
+	(build_scan tables)
+)))
--- lib/sql.scm
+++ lib/sql.scm
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2023  Carl-Philip Hänsch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /* helper functions to work with maps */
 (define map_assoc (lambda (columns fn)
 	(match columns
@@ -13,140 +30,8 @@
 	)
 ))
 
-(define parse_sql (lambda (s) (begin
-	/* lots of small parsers that can be combined */
-	(define identifier (lambda (s) (match s
-		(regex "(?is)^(?:\\s|\\n)*`(.*)`(.*)" _ id rest) '(id rest)
-		(regex "(?is)^(?:\\s|\\n)*([a-zA-Z_][a-zA-Z_0-9]*)(.*)" _ id rest) '(id rest)
-		(error (concat "expected identifier, found " s))
-	)))
-
-	(define parenthesis (lambda (s) (match s
-		(regex "(?is)^(?:\\s|\\n)*\((?:\\s|\\n)*(.*)" _ rest) rest
-		(error (concat "expected (, found " s))
-	)))
-
-	(define tabledecl (lambda (s) (match s
-		(concat ")" rest) '() /* TODO: rest??? */
-		rest (match (identifier rest)
-			'(colname rest) (match (identifier rest)
-				'(typename rest) (match rest
-					/* todo: allow white spaces in dimension */
-					(regex "^(?s)\\(([0-9]+),([0-9]+)\\)([^,]*),(.*)" _ dim1 dim2 typeparams rest) (cons '((symbol "list") colname typename '((symbol "list") dim1 dim2) typeparams) (tabledecl rest))
-					(regex "^(?s)\\(([0-9]+),([0-9]+)\\)([^,]*)\)(.*)" _ dim1 dim2 typeparams rest) '('((symbol "list") colname typename '((symbol "list") dim1 dim2) typeparams)) /* TODO: rest */
-					(regex "^(?s)\\(([0-9]+)\\)([^,]*),(.*)" _ dim1 typeparams rest) (cons '((symbol "list") colname typename '((symbol "list") dim1) typeparams) (tabledecl rest))
-					(regex "^(?s)\\(([0-9]+)\\)([^,]*)\)(.*)" _ dim1 typeparams rest) '('((symbol "list") colname typename '((symbol "list") dim1) typeparams)) /* TODO: rest */
-					(regex "^(?s)([^,]*),(.*)" _ typeparams rest) (cons '((symbol "list") colname typename '((symbol "list")) typeparams) (tabledecl rest))
-					(regex "^(?s)([^,]*)\)(.*)" _ typeparams rest) '('((symbol "list") colname typename '((symbol "list")) typeparams)) /* TODO: rest */
-					(error (concat "expected , or ) but found " rest))
-				)
-			)
-		)
-	)))
-
-	/* eat a identifier from string */
-	(define expression (lambda (s) (match s
-		/* constant */
-		(regex "^(-?[0-9]+(?:\\.[0-9*])?)(?:\\s|\\n)*($|[^0-9].*)" _ num rest) (expression_extend (simplify num) rest)
-		/* identifier (TODO: tblalias.identifier) */
-		(regex "(?is)^(?:\\s|\\n)*`(.*)`(?:\\s|\\n)*(.*)" _ id rest) (expression_extend '((quote get_column) "*" id) rest)
-		(regex "(?is)^(?:\\s|\\n)*([a-zA-Z_][a-zA-Z_0-9]*)(?:\\s|\\n)*(.*)" _ id rest) (expression_extend '((quote get_column) "*" id) rest)
-		/* parenthesis */
-		(concat "(" rest) (match (expression rest) '(expr (concat ")" rest)) '('((quote begin) expr) rest) (error (concat "expected expression found " rest)))
-		(error (concat "could not parse " s))
-	)))
-
-	/* try to find other operators to add to the expression */
-	(define expression_extend (lambda (expr s) (match s
-		/* + - */
-		(regex "^([+\\-])(?:\\s|\\n)*(.*)" _ operator rest)
-			(match (expression rest) '(expr2 rest) '('((symbol operator) expr expr2) rest))
-		/* * / */
-		(regex "^([*\\/])(?:\\s|\\n)*(.*)" _ operator rest)
-			(match (expression rest) '(expr2 rest)
-				(match expr2
-					/* shift down * and / before + and - */
-					'(+ a b) '('((quote +) '((symbol operator) expr a) b) rest)
-					'(- a b) '('((quote -) '((symbol operator) expr a) b) rest)
-					'('((symbol operator) expr expr2) rest)))
-		'(expr s) /* no extension */
-	)))
-
-	/* derive the description of a column from its expression */
-	(define extract_title (lambda (expr) (match expr
-		'((symbol get_column) "*" col) col
-		'((symbol get_column) tblvar col) (concat tblvar "." col)
-		(cons sym args) /* function call */ (concat (cons sym (map args extract_title)))
-		(concat expr)
-	)))
-
-	/* build queryplan from parsed query */
-	(define build_queryplan (lambda (tables fields) (begin
-		/* parse_sql will compile (get_column tblvar col) into the formulas. we have to replace it with the correct variable */
-		/* tables: '('(alias schema tbl) ...) */
-		/* fields: '(colname expr ...) */
-
-		/* returns a list of '(tblvar col) */
-		(define extract_columns (lambda (expr) (match expr
-			'((symbol get_column) tblvar col) '('(tblvar col))
-			(cons sym args) /* function call */ (merge (map args extract_columns)) /* TODO: use collector */
-			'()
-		)))
-
-		/* changes (get_column tblvar col) into its counterpart */
-		(define replace_columns (lambda (expr) (match expr
-			'((symbol get_column) tblvar col) (symbol col) /* TODO: rename in outer scans */
-			(cons sym args) /* function call */ (cons sym (map args replace_columns))
-			expr /* literals */
-		)))
-
-		/* columns: '('(tblalias colname) ...) */
-		(set columns (merge (extract_assoc fields extract_columns)))
-		(print "cols=" fields)
-
-		/* TODO: sort tables according to join plan */
-		(define build_scan (lambda (tables)
-			(match tables
-				'('("1x1" "system" "1x1")) '((symbol "resultrow") (cons (symbol "list") fields))
-				(cons '(alias schema tbl) tables) /* outer scan */
-					'((quote scan) schema tbl
-						'((quote lambda) '() (quote true)) /* TODO: filter */
-						/* todo filter columns for alias */
-						'((quote lambda) (map columns (lambda(column) (match column '(tblvar colname) (symbol colname)))) (build_scan tables))
-						/* TODO: reduce+neutral */)
-				'() /* final inner */ '((symbol "resultrow") (cons (symbol "list") (map_assoc fields replace_columns)))
-			)
-		))
-		(build_scan tables)
-	)))
-	/* compile select */
-	(define select (lambda (rest fields) (begin
-		(define parse_afterexpr (lambda (expr id rest) (match rest
-			/* no FROM */ "" (build_queryplan '('("1x1" "system" "1x1")) (append fields id expr))
-			/* followed by comma: */ (regex "^(?s),(?:\\s|\\n)*(.*)" _ rest) (select rest (append fields id expr))
-			/* followed by AS: */ (regex "^(?is)AS(?:\\s|\\n)*(.*)" _ rest) (match (identifier rest) '(id rest) (parse_afterexpr expr id rest) (error (concat "expected identifier after AS, found: " rest)))
-			/* followed by FROM: */ (regex "^(?is)FROM(?:\\s|\\n)*(.*)" _ rest) (match (identifier rest)
-				'(tblid rest) (build_queryplan '('(tblid (quote schema) tblid)) (append fields id expr))
-				/* TODO: FROM () AS tbl | tbl | tbl as alias ... | comma tablelist */
-			) /* TODO: FROM, WHERE, GROUP usw. */
-			/* otherwise */ (error (concat "expected , AS or FROM but found: " rest))
-		)))
-
-		/* after select, there must be an expression */
-		(match
-			(expression rest) '(expr rest) (parse_afterexpr expr (extract_title expr) rest)
-			(error (concat "expected expression, found " rest)))
-	)))
-
-	(match s
-		(regex "(?s)^\\s*(?m:--.*?$)(.*)" _ rest) /* comment */ (parse_sql rest)
-		(concat "\n" rest) (parse_sql rest)
-		(regex "(?is)^\\s+(.*)" _ rest) (parse_sql rest)
-		(regex "(?is)^CREATE(?:\\s|\\n)+TABLE(?:\\s|\\n)+(.*)" _ rest) (match (identifier rest) '(id rest) '((symbol "createtable") (quote schema) id (cons (symbol "list") (tabledecl (parenthesis rest)))) (error "expected identifier"))
-		(regex "(?is)^SELECT(?:\\s|\\n)+(.*)" _ rest) (select rest '())
-		(error (concat "unknown SQL syntax: " s))
-	)
-)))
+(import "sql-parser.scm")
+(import "queryplan.scm")
 
 /* TODO: session state handling -> which schema */
 (createdatabase "test")
