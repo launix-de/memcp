0f17f5a0771c679c64c5033e0f84d4d7725efeb9
Carl-Philip Hänsch <carli@launix.de>
2024-03-10 15:10:38 +0100

    Fix Schemalock

--- storage/table.go
+++ storage/table.go
@@ -144,13 +144,13 @@ func (d dataset) Get(key string) scm.Scmer {
 }
 
 func (t *table) CreateColumn(name string, typ string, typdimensions[] int, extrainfo string) {
-	t.schemalock.Lock()
+	t.schema.schemalock.Lock()
 	t.Columns = append(t.Columns, column{name, typ, typdimensions, extrainfo})
 	for i := range t.Shards {
 		t.Shards[i].columns[name] = new (StorageSCMER)
 	}
 	t.schema.save()
-	t.schemalock.Unlock()
+	t.schema.schemalock.Unlock()
 }
 
 func (t *table) Insert(d dataset) {
