905889f7a3f1d92714f3ef460301e8cdddecbfe4
Carl-Philip Hänsch <carl-philip.haensch@dzastro.de>
2024-03-27 06:20:15 +0100

    Fix string regex

--- lib/sql-parser.scm
+++ lib/sql-parser.scm
@@ -29,7 +29,7 @@ Copyright (C) 2023, 2024  Carl-Philip Hänsch
 
 (define sql_int (parser (define x (regex "-?[0-9]+")) (simplify x)))
 
-(define sql_string (parser '("'" (define x (regex "(\\.|[^\\'])*" false false)) "'") (replace x "\'" "'")))
+(define sql_string (parser '("'" (define x (regex "(\\\\.|[^\\'])*" false false)) "'") (replace x "\'" "'")))
 
 (define parse_sql (lambda (schema s) (begin
 
