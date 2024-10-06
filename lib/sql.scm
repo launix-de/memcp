d52ea1174646c53a08d2b07ffbee2d852c8a009d
Carl-Philip Hänsch <carli@launix.de>
2024-10-06 13:05:25 +0200

    change ports with envvars

--- lib/sql.scm
+++ lib/sql.scm
@@ -70,9 +70,10 @@ Copyright (C) 2023  Carl-Philip Hänsch
 	))
 ))
 
-/* dedicated mysql protocol listening at port 3307 */
+/* dedicated mysql protocol listening at specified port */
 (try (lambda () (begin
-	(mysql 3307
+	(set port (env "MYSQL_PORT" "3307"))
+	(mysql port
 		(lambda (username_) (scan "system" "user" '("username") (lambda (username) (equal? username username_)) '("password") (lambda (password) password) (lambda (a b) b) nil)) /* auth: load pw hash from system.user */
 		(lambda (username schema) (list? (show schema))) /* switch schema (TODO check grants; in the moment, only the existence of the database is checked) */
 		(lambda (schema sql resultrow_sql session) (begin /* sql */
@@ -82,5 +83,5 @@ Copyright (C) 2023  Carl-Philip Hänsch
 			(eval (source "SQL Query" 1 1 formula))
 		))
 	)
-	(print "MySQL server listening on port 3307 (connect with `mysql -P 3307 -u root -p` using password 'admin')")
+	(print "MySQL server listening on port " port " (connect with `mysql -P " port " -u root -p` using password 'admin'), set with envvar MYSQL_PORT")
 )) print)
